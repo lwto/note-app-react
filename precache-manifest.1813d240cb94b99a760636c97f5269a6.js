088b86f29f7bab6daded03afa9007f4e8c7f1e19
lwto <linwathanoo227@gmail.com>
2022-03-22 20:47:23 +0630

    Updates

---
+++ precache-manifest.1813d240cb94b99a760636c97f5269a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bb3c49d945cc052af23",
+    "url": "/note-app-react/static/css/main.7c086c3a.chunk.css"
+  },
+  {
+    "revision": "8bb3c49d945cc052af23",
+    "url": "/note-app-react/static/js/main.8bb3c49d.chunk.js"
+  },
+  {
+    "revision": "b0bc3c961ffdc100de5f",
+    "url": "/note-app-react/static/js/1.b0bc3c96.chunk.js"
+  },
+  {
+    "revision": "5e576f4f0ba7ff0dad22",
+    "url": "/note-app-react/static/js/runtime~main.5e576f4f.js"
+  },
+  {
+    "revision": "92dc2c0b5427f944f3691b91bba06f88",
+    "url": "/note-app-react/index.html"
+  }
+];
